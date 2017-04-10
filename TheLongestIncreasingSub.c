ea8a570054e0acb7f8b4b81fc754d956ca004de0
Adrian Statescu <mergesortv@gmail.com>
2017-04-09 21:47:38 -0700

    Create TheLongestIncreasingSub.c

---
+++ TheLongestIncreasingSub.c
@@ -0,0 +1,34 @@
+//my solution to HackerRank Problem where Complexity Time is O^2 using Dynamic Programming
+//Terminated doe to Timeout
+
+#include <stdio.h>
+#define MAXS 1000000
+
+int main() {
+   int i, j, N, max,
+          arr[MAXS], 
+           dp[MAXS];
+
+ 
+   scanf("%d", &N);
+
+   for(i = 0; i < N; ++i) scanf("%d", &arr[i]);
+
+   for(i = 0; i < N; ++i) dp[i] = 1;
+
+   for(i = 1; i < N; ++i) {
+
+       for(j = 0; j < i; ++j) {
+
+           if((arr[i]) > (arr[j]) && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+       }
+   }
+
+   max = -1;
+
+   for(i = 0; i < N; ++i) if(max < dp[i]) max = dp[i]; 
+
+   printf("%d", max);
+
+   return(0);
+}
